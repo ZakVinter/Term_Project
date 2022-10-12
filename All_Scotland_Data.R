b1a87acf769551db3260c622d60106aed0797f52
ZakVinter <zvinterusa@outlook.com>
2022-10-12 11:10:21 -0600

    Update All_Scotland_Data.R

--- All_Scotland_Data.R
+++ All_Scotland_Data.R
@@ -1,9 +1,9 @@
 
 
+url = "https://www.transfermarkt.us/scottish-premiership/topErhalteneElfmeter/wettbewerb/SC1/plus/1?saison_id=2021"
 
 
-
-
+selector = "#main > main > div:nth-child(7) > div.large-8.columns > div > div.responsive-table"
 
 
 
