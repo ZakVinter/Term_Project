dcc67a8b5e52e50399dfe8518f0f9a1d7bd8e169
ZakVinter <zvinterusa@outlook.com>
2022-12-06 20:13:49 -0700

    --

--- R_Code/Econometric Analysis/1_Reg_Data_Creation.R
+++ R_Code/Econometric Analysis/1_Reg_Data_Creation.R
@@ -1,28 +1,37 @@
+
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(tidyverse)
 
 
 
-
+#load in data needed for creating regression data sets.
 load("~/GitHub/Term_Project/R_Data/Joined/Appended_Joined.Rdata")
 
 
 
+#######################################
 
 
+
+
+#remove unnecessary columns
 Reg_Data = Appended_Joined[, -5:-8]
 
 
 
-
-
-
+#create a dummy variable for observations later than the treatment
 Reg_Data$Time = ifelse(Reg_Data$YEAR >= 2019, 1, 0)
 
 
+
+
+#create a dummy variable for Country. It shows 1 if England and 0 if Scotland
 Reg_Data$Area = ifelse(Reg_Data$COUNTRY == "England", 1, 0)
 
 
+
+
+#create interaction term
 Reg_Data = Reg_Data %>%
   mutate(
     Interaction = Time*Area
@@ -65,16 +74,32 @@ tidy_Top_6 = rbind(Top_6, Not_Top_6)
 
 
 
+
+
+
+
+
+
+
+#remove unnecessary columns
 Reg_Data_2 = tidy_Top_6[, -5:-7]
 
 
 
+
+#create a dummy variable for observations later than the treatment
 Reg_Data_2$Time = ifelse(Reg_Data_2$YEAR >= 2019, 1, 0)
 
 
+
+
+#create a dummy variable for Type. It shows 1 if Top_6 and 0 if not
 Reg_Data_2$Area = ifelse(Reg_Data_2$Type == "Top 6", 1, 0)
 
 
+
+
+#interaction term
 Reg_Data_2 = Reg_Data_2 %>%
   mutate(
     Interaction = Time*Area
@@ -90,7 +115,10 @@ save(Reg_Data_2, file = "Github/Term_Project/R_Data/Econometrics/Reg_Data_2.Rdat
 ####################################################################
 
 
+
+
 load("~/GitHub/Term_Project/R_Data/Scotland/Top_5_Scot.Rdata")
+
 
 
 
@@ -104,7 +132,7 @@ Top_5_Scot = Top_5_Scot %>%
 
 
 
-
+#create a data set with the top teams from both Scotland and England
 Tidy_Top_Teams = rbind(Top_5_Scot, Top_6)
 
 
@@ -118,20 +146,26 @@ save(Tidy_Top_Teams, file = "Github/Term_Project/R_Data/Joined/Tidy_Top_Teams.Rd
 
 
 
-
+#remove unnecessary columns
 Reg_Data_3 = Tidy_Top_Teams[, -5:-7]
 
 
 
 
 
-
+#create a dummy variable for observations later than the treatment
 Reg_Data_3$Time = ifelse(Reg_Data_3$YEAR >= 2019, 1, 0)
 
 
+
+
+#create a dummy variable for Country. It shows 1 if England and 0 if Scotland
 Reg_Data_3$Area = ifelse(Reg_Data_3$COUNTRY == "England", 1, 0)
 
 
+
+
+#interaction term
 Reg_Data_3 = Reg_Data_3 %>%
   mutate(
     Interaction = Time*Area
